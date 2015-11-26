a804d20d0ba89a8b685fc25cb97711dbdeea4e20
Nathan Cerny <ncerny@chef.io>
2015-11-25 23:30:14 -0600

    Update URI to internal chef server.

--- attributes/chef-client.rb
+++ attributes/chef-client.rb
@@ -3,5 +3,5 @@ default['chef-client']['splay']  = '300'
 default['chef-client']['log_dir']   = '/var/log/chef'
 default['chef-client']['log_file']  = 'chef-client.log'
 
-default['chef-client']['config']['chef_server_url'] = 'https://api.opscode.com/organizations/cerny'
+default['chef-client']['config']['chef_server_url'] = 'https://chef.cerny.cc/organizations/cerny'
 default['chef-client']['config']['ssl_verify_mode'] = ':verify_peer'
